63c9a6edb1f928d281ff89128d7d1dc4018449f4
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-30 03:01:51 +0700

    setup navbar-menu scaffold

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
 
 setup navbar scaffold
 
-```html
+```jsx
     <div className="gpt__navbar">
       <div className="gpt__navbar-links">
         <div className="gpt__navbar-links_logo">
@@ -38,4 +38,40 @@ setup navbar scaffold
         <button type="button">Sign up</button>
       </div>
     </div>
+```
+
+add hamburgur navbar menu
+
+    add react icons and usestate
+    import React, { useState } from 'react';
+    import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+
+    because use state had to add 'use client' on the top
+    set initial state
+      const [toggleMenu, setToggleMenu] = useState(false);
+
+    setup navbar-menu scaffold
+
+```jsx
+      <div className="gpt3__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="gpt3__navbar-menu_container scale-up-center">
+          <div className="gpt3__navbar-menu_container-links">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+          </div>
+          <div className="gpt3__navbar-menu_container-links-sign">
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
+          </div>
+        </div>
+        )}
+      </div>
+
 ```
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+'use client';
+
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="flex justify-between items-center py-8 px-24">
       <div className="flex flex-1 justify-start items-center">
@@ -20,6 +25,29 @@ const Navbar = () => {
       <p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer">Sign in</p>
         <button className="py-2 px-4 text-white bg-red-600 font-medium text-lg leading-6 border-none outline-none cursor-pointer rounded" type="button">Sign up</button>
       </div>
+
+      {/* navbar menu  */}
+      <div className="gpt3__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="gpt3__navbar-menu_container scale-up-center">
+          <div className="gpt3__navbar-menu_container-links">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+          </div>
+          <div className="gpt3__navbar-menu_container-links-sign">
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
+          </div>
+        </div>
+        )}
+      </div>
+
     </div>
   )
 }
