52029b9a4e85a02c2711f78e31dff71cc7860ae3
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-30 02:28:22 +0700

    setup navbar scaffold

--- README.md
+++ README.md
@@ -13,3 +13,29 @@
     <Footer />
   </div>
 ```
+
+
+## navbar
+
+setup navbar scaffold
+
+```html
+    <div className="gpt__navbar">
+      <div className="gpt__navbar-links">
+        <div className="gpt__navbar-links_logo">
+          <img src="logo.svg" />
+        </div>
+        <div className="gpt__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt">What is GPT?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className="gpt__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+    </div>
+```
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="gpt__navbar">
+      <div className="gpt__navbar-links">
+        <div className="gpt__navbar-links_logo">
+          <img src="logo.svg" />
+        </div>
+        <div className="gpt__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt">What is GPT?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className="gpt__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+    </div>
   )
 }
 
