5c5067aa2ad7b760c0bdf7297359f71cea4d33b9
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-29 18:44:29 +0700

    add scaffold html and file structure

---
+++ README.md
@@ -0,0 +1,16 @@
+## get the scafhold in page.jsx
+
+    <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <WhatGPT />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 
+// import { Footer, Blog, Possibility, Features, WhatGPT, Header } from '@/containers';
+// import { CTA, Brand, Navbar } from '@/components';
+
+import Header from '@/containers/Header'
+import WhatGPT from '@/containers/WhatGPT'
+import Features from '@/containers/Features'
+import Possibility from '@/containers/Possibility'
+import Blog from '@/containers/Blog'
+import Footer from '@/containers/Footer'
+
+import Navbar from '@/components/Navbar'
+import Brand from '@/components/Brand'
+import CTA from '@/components/CTA'
+
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <WhatGPT />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
   )
 }
 
