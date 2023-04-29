a81e2bcb25c7eb47a486343a015bcbbaa7e09c09
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-30 03:52:31 +0700

    finish styling for navbar-menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,9 +11,9 @@ const Navbar = () => {
     <div className="flex justify-between items-center py-8 px-24">
       <div className="flex flex-1 justify-start items-center">
         <div className="mr-8">
-          <img className="w-16 h-4" src="logo.svg"/>
+          <img className="w-16 h-4 cursor-pointer" src="logo.svg"/>
         </div>
-        <div className="flex flex-row">
+        <div className="hidden lg:flex lg:flex-row">
           <p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer"><a href="#home">Home</a></p>
           <p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer"><a href="#wgpt">What is GPT?</a></p>
           <p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer"><a href="#possibility">Open AI</a></p>
@@ -21,28 +21,28 @@ const Navbar = () => {
           <p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer"><a href="#blog">Library</a></p>
         </div>
       </div>
-      <div className="flex justify-end items-center">
-      <p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer">Sign in</p>
+      <div className="hidden md:flex md:justify-end md:items-center">
+        <p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer">Sign in</p>
         <button className="py-2 px-4 text-white bg-red-600 font-medium text-lg leading-6 border-none outline-none cursor-pointer rounded" type="button">Sign up</button>
       </div>
 
       {/* navbar menu  */}
-      <div className="gpt3__navbar-menu">
+      <div className="ml-4 flex relative lg:hidden">
         {toggleMenu
           ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
           : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
         {toggleMenu && (
-        <div className="gpt3__navbar-menu_container scale-up-center">
+        <div className="flex justify-end items-end flex-col bg-gray-900 p-8 absolute right-0 mt-4 rounded text-end top-10 min-w-[210px] shadow-sm scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
-            <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT3?</a></p>
-            <p><a href="#possibility">Open AI</a></p>
-            <p><a href="#features">Case Studies</a></p>
-            <p><a href="#blog">Library</a></p>
+            <p className="text-white font-medium text-lg leading-6 capitalize my-4 mx-0 cursor-pointer"><a href="#home">Home</a></p>
+            <p className="text-white font-medium text-lg leading-6 capitalize my-4 mx-0 cursor-pointer"><a href="#wgpt">What is GPT?</a></p>
+            <p className="text-white font-medium text-lg leading-6 capitalize my-4 mx-0 cursor-pointer"><a href="#possibility">Open AI</a></p>
+            <p className="text-white font-medium text-lg leading-6 capitalize my-4 mx-0 cursor-pointer"><a href="#features">Case Studies</a></p>
+            <p className="text-white font-medium text-lg leading-6 capitalize my-4 mx-0 cursor-pointer"><a href="#blog">Library</a></p>
           </div>
-          <div className="gpt3__navbar-menu_container-links-sign">
-            <p>Sign in</p>
-            <button type="button">Sign up</button>
+          <div className="block md:hidden">
+            <p className="text-white font-medium text-lg leading-6 capitalize my-4 mx-0 cursor-pointer">Sign in</p>
+            <button className="py-2 px-4 text-white bg-red-600 font-medium text-lg leading-6 border-none outline-none cursor-pointer rounded" type="button">Sign up</button>
           </div>
         </div>
         )}
