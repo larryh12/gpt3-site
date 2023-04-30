ce1708561bca0b82d2fa197fbd290a4c20d824e5
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-30 20:21:38 +0700

    add brand section scaffold

--- components/Brand.jsx
+++ components/Brand.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const Brand = () => {
-  return (
-    <div>Brand</div>
-  )
-}
+
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src="google.png" />
+    </div>
+    <div>
+      <img src="slack.png" />
+    </div>
+    <div>
+      <img src="atlassian.png" />
+    </div>
+    <div>
+      <img src="dropbox.png" />
+    </div>
+    <div>
+      <img src="shopify.png" />
+    </div>
+  </div>
+);
 
 export default Brand
