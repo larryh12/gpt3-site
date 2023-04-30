7cccca950a9349379964a803338b442e85da2d6a
Larry H <111525408+larryh12@users.noreply.github.com>
2023-04-30 14:38:26 +0700

    refactor css

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,46 +7,43 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div className="flex justify-between items-center py-8 px-24">
-      <div className="flex flex-1 justify-start items-center">
-        <div className="mr-8">
-          <img className="w-16 h-4 cursor-pointer" src="logo.svg"/>
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src="logo.svg" />
         </div>
-        <div className="hidden lg:flex lg:flex-row">
-          <p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer"><a href="#home">Home</a></p>
-          <p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer"><a href="#wgpt">What is GPT?</a></p>
-          <p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer"><a href="#possibility">Open AI</a></p>
-          <p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer"><a href="#features">Case Studies</a></p>
-          <p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer"><a href="#blog">Library</a></p>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
         </div>
       </div>
-      <div className="hidden md:flex md:justify-end md:items-center">
-        <p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer">Sign in</p>
-        <button className="py-2 px-4 text-white bg-red-600 font-medium text-lg leading-6 border-none outline-none cursor-pointer rounded" type="button">Sign up</button>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
       </div>
-
-      {/* navbar menu  */}
-      <div className="ml-4 flex relative lg:hidden">
+      <div className="gpt3__navbar-menu">
         {toggleMenu
           ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
           : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
         {toggleMenu && (
-        <div className="flex justify-end items-end flex-col bg-gray-900 p-8 absolute right-0 mt-4 rounded text-end top-10 min-w-[210px] shadow-sm scale-up-center">
+        <div className="gpt3__navbar-menu_container scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
-            <p className="text-white font-medium text-lg leading-6 capitalize my-4 mx-0 cursor-pointer"><a href="#home">Home</a></p>
-            <p className="text-white font-medium text-lg leading-6 capitalize my-4 mx-0 cursor-pointer"><a href="#wgpt">What is GPT?</a></p>
-            <p className="text-white font-medium text-lg leading-6 capitalize my-4 mx-0 cursor-pointer"><a href="#possibility">Open AI</a></p>
-            <p className="text-white font-medium text-lg leading-6 capitalize my-4 mx-0 cursor-pointer"><a href="#features">Case Studies</a></p>
-            <p className="text-white font-medium text-lg leading-6 capitalize my-4 mx-0 cursor-pointer"><a href="#blog">Library</a></p>
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
           </div>
-          <div className="block md:hidden">
-            <p className="text-white font-medium text-lg leading-6 capitalize my-4 mx-0 cursor-pointer">Sign in</p>
-            <button className="py-2 px-4 text-white bg-red-600 font-medium text-lg leading-6 border-none outline-none cursor-pointer rounded" type="button">Sign up</button>
+          <div className="gpt3__navbar-menu_container-links-sign">
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
           </div>
         </div>
         )}
       </div>
-
     </div>
   )
 }
