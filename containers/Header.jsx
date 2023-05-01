1f95b20d193f864656b6882a3b1bb63efabdbfa3
Larry H <111525408+larryh12@users.noreply.github.com>
2023-05-01 17:22:22 +0700

    misc text format fix

--- containers/Header.jsx
+++ containers/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
+      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT OpenAI</h1>
       <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
       <div className="gpt3__header-content__input">
