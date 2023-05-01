f60eb36fa0543808b5b3cdda88c0f687ed59fabb
Larry H <111525408+larryh12@users.noreply.github.com>
2023-05-01 14:37:22 +0700

    add features section layout

--- containers/Features.jsx
+++ containers/Features.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Feature from '@/components/Feature'
 
-const Features = () => {
-  return (
-    <div>Features</div>
-  )
-}
+const Features = () => (
+  <div className="gpt3__features section__padding" id="features">
+    <div className="gpt3__features-heading">
+      <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
+      <p>Request Early Access to Get Started</p>
+    </div>
+    <div className="gpt3__features-container">
+      {featuresData.map((item, index) => (
+        <Feature title={item.title} text={item.text} key={item.title + index} />
+      ))}
+    </div>
+  </div>
+);
 
 export default Features
