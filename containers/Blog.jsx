bf40b2cb5c93e9af6be020d16df87e29d709acb6
Larry H <111525408+larryh12@users.noreply.github.com>
2023-05-01 15:48:56 +0700

    add blog section layout structure

--- containers/Blog.jsx
+++ containers/Blog.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
-}
+const Blog = () => (
+  <div className="gpt3__blog section__padding" id="blog">
+    <div className="gpt3__blog-heading">
+      <h1 className="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
+    </div>
+    <div className="gpt3__blog-container">
+      <div className="gpt3__blog-container_groupA">
+        <Article imgUrl="blog1.png" date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+      </div>
+      <div className="gpt3__blog-container_groupB">
+        <Article imgUrl="blog2.png" date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl="blog3.png" date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl="blog4.png" date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl="blog5.png" date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+      </div>
+    </div>
+  </div>
+);
 
 export default Blog
